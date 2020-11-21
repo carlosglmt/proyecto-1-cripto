8afb3a6b4f192c1efaa317ae1850717c11d27a61
ALM <andreslopezmartinez@hotmail.com>
2020-11-20 22:38:35 -0600

    cambios

--- program.py
+++ program.py
@@ -3,14 +3,38 @@ import fileinput
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA384, SHA512
 
-def doAES(filename, decryption_mode=False):
+def doAES_EBC(filename, decryption_mode=False):
     line_counter = 0
     for line in fileinput.input(filename):
         if line_counter == 0:
             key = bytearray.fromhex(line.rstrip())
         elif line_counter == 1:
             data = bytearray.fromhex(line.rstrip())
-            if decryption_mode == False:
+            if not decryption_mode:
+                # Aqui medir el tiempo
+                cipher = AES.new(key, AES.MODE_ECB)
+                data_output = cipher.encrypt(data)
+                # Aqui ya no medir el tiempo
+            else:
+                # Aqui medir el tiempo
+                cipher = AES.new(key, AES.MODE_ECB)
+                data_output = cipher.decrypt(data)
+                # Aqui ya no medir el tiempo
+
+            # Imprime texto cifrado
+            for i in range(len(data_output)):
+                print('{:0>2X}'.format(data_output[i]), end = '')
+            print("")
+        line_counter = (line_counter + 1) % 3
+
+def doAES_CBC(filename, decryption_mode=False):
+    line_counter = 0
+    for line in fileinput.input(filename):
+        if line_counter == 0:
+            key = bytearray.fromhex(line.rstrip())
+        elif line_counter == 1:
+            data = bytearray.fromhex(line.rstrip())
+            if not decryption_mode:
                 # Aqui medir el tiempo
                 cipher = AES.new(key, AES.MODE_ECB)
                 data_output = cipher.encrypt(data)
@@ -46,20 +70,30 @@ def doSHA(filename, length):
 """
 AES-ECB256 BLOCK 128bits
 ENCRYPT
-Linea 0: llave
-Linea 1: plaintext
-Linea 2: ciphertext
+Line 0: key
+Line 1: plaintext
+Line 2: ciphertext
 """
-doAES("./AES-ECB-256/vectores")
+doAES_EBC("./AES-ECB-256/vectores")
 
 """
 AES-ECB256 BLOCK 128bits
 DECRYPT
-Linea 0: llave
-Linea 1: ciphertext
-Linea 2: plaintext
+Line 0: key
+Line 1: ciphertext
+Line 2: plaintext
 """
-doAES("./AES-ECB-256/vectores", True)
+doAES_EBC("./AES-ECB-256/vectores", True)
+
+"""
+AES-CBC256
+ENCRYPT
+Line 0: key
+Line 1: IV
+Line 2: plaintext
+Line 3: ciphertext
+"""
+doAES_EBC("./AES-ECB-256/vectores")
 
 """
 SHA384
