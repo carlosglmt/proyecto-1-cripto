7cb2a01d1ec2ee30d21d15c6f8a33a649c8bc4b7
Miguel Angel Perez Quiroz <sindro86@gmail.com>
2020-11-20 19:59:42 -0600

    codigo

--- program.py
+++ program.py
@@ -1,13 +1,44 @@
 import sys
+import fileinput
 from Crypto.Cipher import AES
 
-a = "139a35422f1d61de3c91787fe0507afd"
-b = "b9145a768b7dc489a096b546f43b231f"
-#key = bytes.fromhex(a)
+def doAES(filename, decryption_mode=False):
+    line_counter = 0
+    for line in fileinput.input(filename):
+        if line_counter == 0:
+            key = bytearray.fromhex(line.rstrip())
+        elif line_counter == 1:
+            data = bytearray.fromhex(line.rstrip())
+            if decryption_mode == False:
+                # Aqui medir el tiempo
+                cipher = AES.new(key, AES.MODE_ECB)
+                data_output = cipher.encrypt(data)
+                # Aqui ya no medir el tiempo
+            else:
+                # Aqui medir el tiempo
+                cipher = AES.new(key, AES.MODE_ECB)
+                data_output = cipher.decrypt(data)
+                # Aqui ya no medir el tiempo
 
-cipher = AES.new(b'139a35422f1d61de3c91787fe0507afd', AES.MODE_ECB)
-ct_bytes = cipher.encrypt(b'b9145a768b7dc489a096b546f43b231f')
-
-for i in range(len(ct_bytes)):
-    print('{:0>2x}'.format(ct_bytes[i]), end = '')
-print("")
+            # Imprime texto cifrado
+            for i in range(len(data_output)):
+                print('{:0>2X}'.format(data_output[i]), end = '')
+            print("")
+        line_counter = (line_counter + 1) % 3
+    
+"""
+AES-ECB256 BLOCK 128bits
+ENCRYPT
+Linea 0: llave
+Linea 1: plaintext
+Linea 2: ciphertext
+"""
+#doAES("./AES-ECB-256/CIFRADO/vectores", True)
+"""
+AES-ECB256 BLOCK 128bits
+DECRYPT
+Linea 0: llave
+Linea 1: ciphertext
+Linea 2: plaintext
+"""
+#doAES("./AES-ECB-256/DESCIFRADO/vectores", True)
